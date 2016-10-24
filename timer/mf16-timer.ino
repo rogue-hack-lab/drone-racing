2588931ac649951067088a14fb27c0c67da6ab27
Kevin Conner <kevin.r.conner@gmail.com>
2016-10-23 21:35:17 -0700

    Minimum usable state achieved! it times, starts, and stops!

--- timer/mf16-timer.ino
+++ timer/mf16-timer.ino
@@ -25,6 +25,7 @@ void segmentTest(Adafruit_7segment matrix);
 #define LANE_1_BTN_PIN D3
 #define LANE_2_BTN_PIN D4
 #define LANE_3_BTN_PIN D5
+
 Button startBtn = Button(START_BTN_PIN, BUTTON_PULLDOWN_INTERNAL);
 Button laneBtn[LANES] = {
   Button(LANE_1_BTN_PIN, BUTTON_PULLDOWN_INTERNAL),
@@ -33,6 +34,11 @@ Button laneBtn[LANES] = {
 };
 
 Adafruit_7segment lcd[LANES];
+
+uint32_t counter[LANES];
+bool laneActive[LANES];
+uint32_t startTime = 0;
+
 
 void setup() {
   for (int i; i < LANES; i++){
@@ -44,39 +50,35 @@ void setup() {
   lcd[2].begin(0x72);
 
   for (int i; i < LANES; i++){
-    lcd[i].println(i + 1);
+    lcd[i].writeDigitNum(0, 0, true);
+    lcd[i].writeDigitNum(1, 0, false);
+    lcd[i].writeDigitNum(3, 0, true);
+    lcd[i].writeDigitNum(4, 0, false);
     lcd[i].writeDisplay();
   }
 }
 
-uint16_t counter[LANES];
-bool laneActive[LANES];
-uint32_t startTime = 0;
-
 void loop() {
-
     doStartButton();
-
     doLaneButtons();
-
     delay(10);
-
 }
 
 void doStartButton() {
   if (startBtn.uniquePress()){
       Particle.publish("StartButton","Start");
-      startTime = millis();
       startRace();
+      while (startBtn.isPressed()){}
+      startTime = millis();
   }
 }
 
 void doLaneButtons() {
   for (int i; i < LANES; i++){
     if (laneActive[i]) {
-      counter[i] = formatTime(millis() - startTime);
-      lcd[i].println(counter[i]);
-      lcd[i].writeDisplay();
+      formatTime(i, millis() - startTime);
+      //lcd[i].println(counter[i]);
+      //lcd[i].writeDisplay();
       if (laneBtn[i].uniquePress()){
           Particle.publish("LaneStop",(String)(i));
           laneActive[i] = !laneActive[i];
@@ -90,65 +92,24 @@ void startRace(){
   for (int i; i < LANES; i++){
     counter[i] = 0;
     laneActive[i] = true;
-    lcd[i].println(counter[i]);
+    lcd[i].writeDigitNum(0, 0, true);
+    lcd[i].writeDigitNum(1, 0, false);
+    lcd[i].writeDigitNum(3, 0, true);
+    lcd[i].writeDigitNum(4, 0, false);
     lcd[i].writeDisplay();
   }
 }
 
-uint16_t formatTime(uint16_t milliseconds){
-  uint16_t minutes = (int) (((milliseconds / (1000 * 60)) % 10) * 1000);
-  uint16_t seconds = (int) ((milliseconds / 1000) * 10);
-  uint16_t tenths  = (int) ((milliseconds / 100) % 10);
-  return minutes + seconds + tenths;
-}
+void formatTime(int lane, uint32_t milliseconds){
+  uint32_t minutes = (uint8_t)((milliseconds / 60000) % 10);
+  milliseconds %= 60000;
+  uint32_t seconds = (uint8_t)(milliseconds / 1000);
+  milliseconds %=  1000;
+  uint32_t tenths  = (uint8_t)((milliseconds / 100) % 10);
 
-
-
-
-
-// for reference and testing
-void segmentTest(Adafruit_7segment matrix) {
-  /*// try to print a number thats too long
-  matrix.print(10000, DEC);
-  matrix.writeDisplay();
-  delay(500);
-
-  // print a hex number
-  matrix.print(0xBEEF, HEX);
-  matrix.writeDisplay();
-  delay(500);
-
-  // print a floating point
-  matrix.print(12.34);
-  matrix.writeDisplay();
-  delay(500);
-
-  // print with print/println
-  for (uint16_t counter = 0; counter < 9999; counter++) {
-    matrix.println(counter);
-    matrix.writeDisplay();
-    delay(10);
-  }*/
-
-  // method #2 - draw each digit
-  uint16_t blinkcounter = 0;
-  boolean drawDots = false;
-  for (uint16_t counter = 0; counter < 9999; counter ++) {
-    matrix.writeDigitNum(0, (counter / 1000), drawDots);
-    matrix.writeDigitNum(1, (counter / 100) % 10, drawDots);
-    matrix.drawColon(drawDots);
-    matrix.writeDigitNum(3, (counter / 10) % 10, drawDots);
-    matrix.writeDigitNum(4, counter % 10, drawDots);
-
-    blinkcounter+=50;
-    if (blinkcounter < 500) {
-      drawDots = false;
-    } else if (blinkcounter < 1000) {
-      drawDots = true;
-    } else {
-      blinkcounter = 0;
-    }
-    matrix.writeDisplay();
-    delay(10);
-  }
+  lcd[lane].writeDigitNum(0, minutes, true);
+  lcd[lane].writeDigitNum(1, (int) seconds / 10, false);
+  lcd[lane].writeDigitNum(3, seconds % 10, true);
+  lcd[lane].writeDigitNum(4, tenths, false);
+  lcd[lane].writeDisplay();
 }
